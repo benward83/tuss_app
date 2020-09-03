a83172e935da81b237242e631d6002016d0c0086
Ben Ward <benward83@gmail.com>
2020-09-03 15:21:58 +0200

    Addition of pics to Gallery

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -4,33 +4,14 @@
       <div class="row align-items-center content">
         <div class="col-md-6">
           <img
-            src="src/assets/art/Images/Tuss1.jpg"
+            src="../assets/art/Images/Tuss1.jpg"
             alt=""
             class="img-fluid"
           >
         </div>
         <div class="col-md-6">
           <img
-            src="src/assets/art/Images/Tuss2.jpg"
-            alt=""
-            class="img-fluid"
-          >
-        </div>
-      </div>
-    </section>
-
-    <section class="container-fluid">
-      <div class="row align-items-center content">
-        <div class="col-md-6">
-          <img
-            src="src/assets/art/Images/Tuss3.jpg"
-            alt=""
-            class="img-fluid"
-          >
-        </div>
-        <div class="col-md-6">
-          <img
-            src="src/assets/art/Images/Tuss4.jpg"
+            src="../assets/art/Images/Tuss2.jpg"
             alt=""
             class="img-fluid"
           >
@@ -42,14 +23,14 @@
       <div class="row align-items-center content">
         <div class="col-md-6">
           <img
-            src="src/assets/art/Images/Tuss5.jpg"
+            src="../assets/art/Images/Tuss3.jpg"
             alt=""
             class="img-fluid"
           >
         </div>
         <div class="col-md-6">
           <img
-            src="src/assets/art/Images/Tuss6.jpg"
+            src="../assets/art/Images/Tuss4.jpg"
             alt=""
             class="img-fluid"
           >
@@ -61,14 +42,33 @@
       <div class="row align-items-center content">
         <div class="col-md-6">
           <img
-            src="src/assets/art/Images/Tuss7.jpg"
+            src="../assets/art/Images/Tuss5.jpg"
             alt=""
             class="img-fluid"
           >
         </div>
         <div class="col-md-6">
           <img
-            src="src/assets/art/Images/Tuss8.jpg"
+            src="../assets/art/Images/Tuss6.jpg"
+            alt=""
+            class="img-fluid"
+          >
+        </div>
+      </div>
+    </section>
+
+    <section class="container-fluid">
+      <div class="row align-items-center content">
+        <div class="col-md-6">
+          <img
+            src="../assets/art/Images/Tuss7.jpg"
+            alt=""
+            class="img-fluid"
+          >
+        </div>
+        <div class="col-md-6">
+          <img
+            src="../assets/art/Images/Tuss8.jpg"
             alt=""
             class="img-fluid"
           >
